a36564d637dce14ef39472c879c969ae0bfdb525
sapphicme <43253070+sapphicme@users.noreply.github.com>
2018-09-30 12:57:14 -0400

    week 4

---
+++ Assets/Scripts/mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+
+    //Variable Declartations 
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+            rBody.velocity = transform.right * speed; 
+	}
+	
+}
